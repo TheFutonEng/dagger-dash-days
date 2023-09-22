bafbc307ea0cfddb29d2a218a915f2fb51a5ca13
Rob <thefutoneng@gmail.com>
2023-09-22 11:32:54 +0000

    feat: added ability to do OS and architecture detection.

--- src/pkg/zarf/zarf.go
+++ src/pkg/zarf/zarf.go
@@ -3,13 +3,55 @@ package zarf
 import (
 	"fmt"
 	"main/src/pkg/dagger/types"
+	"runtime"
 )
 
 var (
-	wgetZarf    = []string{"wget", "https://github.com/defenseunicorns/zarf/releases/download/v0.29.2/zarf_v0.29.2_Linux_arm64"}
-	chmodZarf   = []string{"chmod", "+x", "zarf_v0.29.2_Linux_arm64"}
-	zarfVersion = []string{"./zarf_v0.29.2_Linux_arm64", "version"}
+	arch, os   string
+	wgetZarf   []string
+	chmodZarf  []string
+	zarfVersion []string
 )
+
+func init() {
+	// Determine CPU architecture
+	switch runtime.GOARCH {
+	case "amd64":
+		arch = "amd64"
+	case "arm64":
+		arch = "arm64"
+	default:
+		arch = "unknown"
+	}
+
+	// Determine OS
+	switch runtime.GOOS {
+	case "linux":
+		os = "Linux"
+	case "darwin":
+		os = "Darwin/OSX"
+	case "windows":
+		os = "Windows"
+	default:
+		os = "unknown"
+	}
+
+	// Construct the variables based on `arch` and `os`
+	version := "v0.29.2"
+	baseURL := "https://github.com/defenseunicorns/zarf/releases/download"
+	filename := fmt.Sprintf("zarf_%s_%s_%s", version, os, arch)
+
+	wgetZarf = []string{"wget", fmt.Sprintf("%s/%s/%s", baseURL, version, filename)}
+	chmodZarf = []string{"chmod", "+x", filename}
+	zarfVersion = []string{"./" + filename, "version"}
+}
+
+// var (
+// 	wgetZarf    = []string{"wget", "https://github.com/defenseunicorns/zarf/releases/download/v0.29.2/zarf_v0.29.2_Linux_arm64"}
+// 	chmodZarf   = []string{"chmod", "+x", "zarf_v0.29.2_Linux_arm64"}
+// 	zarfVersion = []string{"./zarf_v0.29.2_Linux_arm64", "version"}
+	
+// )
 
 func SetupZarf(runner *types.RunnerContainer) {
 	container := runner.Runner
